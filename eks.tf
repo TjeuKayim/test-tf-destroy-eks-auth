1e7f9af50a208735aa6eab4932574936e7de0426
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2021-03-15 15:40:26 +0100

    Configure Helm provider

--- eks.tf
+++ eks.tf
@@ -20,6 +20,14 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "14.0.0"
